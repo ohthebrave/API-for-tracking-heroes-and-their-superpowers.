8b00b144027d165a10d442b5a420324ff8580ec9
George <georgejoj28@gmail.com>
2024-01-14 18:06:18 +0300

    add components

---
+++ client/src/components/App.js
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom"
+import Header from "./Header";
+import Hero from "./Hero";
+import Home from "./Home";
+import HeroPowerForm from "./HeroPowerForm";
+import Power from "./Power";
+import PowerEditForm from "./PowerEditForm";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+     
+        <Routes>
+            <Route exact path="/hero_powers/new" element={<HeroPowerForm />}/>
+            <Route exact path="/powers/:id/edit" element={<PowerEditForm />} />
+            <Route exact path="/powers/:id" element={<Power />}/>
+            <Route exact path="/heroes/:id" element={<Hero />}/>
+            <Route exact path="/" element={<Home />} />
+        </Routes>
+       
+      </main>
+    </div>
+  );
+}
+
+export default App;
